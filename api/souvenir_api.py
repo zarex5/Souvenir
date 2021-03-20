2b55e04f117441f8ad3345f65df1a60dac3dc7b5
Joel Jesudason <62968241+JoelJesudason@users.noreply.github.com>
2021-03-20 17:58:50 -0400

    Added default token generation functionality

--- api/souvenir_api.py
+++ api/souvenir_api.py
@@ -3,6 +3,7 @@ from flask import request
 from dotenv import load_dotenv
 from pathlib import Path
 from flask_cors import CORS
+from PIL import Image, ImageFont, ImageDraw 
 import requests
 import os
 
@@ -14,6 +15,12 @@ headers = {
     "pinata_secret_api_key": os.getenv("PINATA_SECRET_API_KEY"),
 }
 
+default_stamps = {
+    "Template1":Image.open("./test_pics/stamp1.jpg"),
+    "Template2":Image.open("./test_pics/stamp2.jpg"),
+    "Template3":Image.open("./test_pics/stamp3.jpg")
+}
+
 # Function managing Pinata pinning post request
 def pinata_pin(file_dict):
     r = requests.post(
@@ -23,11 +30,39 @@ def pinata_pin(file_dict):
         )
     return r
 
+# Function that puts city name on default stamp
+def put_title(image,city_name,color = "#000000"):
+    # Making image editable
+    image_editable = ImageDraw.Draw(image)
 
-# API functionality
+    # Setting font
+    font_size = int(image_editable.im.size[0]*0.05)
+    title_font = ImageFont.truetype('roboto/Roboto-Bold.ttf',  font_size)
+
+    # Getting font color
+    adjcolor = color.lstrip('#')
+    print(adjcolor)
+    color_tuple = tuple(int(adjcolor[i:i+2], 16) for i in (0, 2, 4))
+
+    half_image_width = image_editable.im.size[0]/2
+    half_text_width = title_font.getsize(city_name)[0]/2
+    half_text_height = title_font.getsize(city_name)[1]/2
+    entire_image_length = image_editable.im.size[1]
+    seventh_image_length = image_editable.im.size[1]*0.07
+
+    # Applying text
+    image_editable.text(
+                        ( half_image_width - half_text_width, entire_image_length - (seventh_image_length + half_text_height) ), # positioning (bottom center)
+                        city_name, # city name
+                        color_tuple, # color
+                        font=title_font # font
+                        )
+    image.save("result.jpg")
+
+########## API functionality ##########
 
 # Pin file via API
-@app.route('/pin-image',methods=['GET','POST'])
+@app.route('/pin-image',methods=['POST'])
 def pin_image():
     print("Posted file: {}".format(request.files['file']))
     file = {'file' : request.files['file'].read()}
@@ -44,10 +79,31 @@ def pin_image():
     # Example POST request:
     ## requests.post("http://localhost:105/pin-image",files = {'file': open(Path('./pics/myface.png'),'rb')})
 
+# Create default token and pin file
+@app.route('/generate-image',methods=['GET','POST'])
+def generate_image():
 
-
+    templateNb = request.args.get('templateNb')
+    cityName = request.args.get('cityName')
+    fontColor = request.args.get('fontColor')
     
+    stamp = default_stamps[templateNb]
+    f = put_title(stamp,cityName,fontColor)
 
+    file = {'file' : open(Path('./result.jpg'),'rb') }
+    r = pinata_pin(file)
+    print(r.text)
+
+    if 'IpfsHash' in r.text:
+        # print(str(r.text))
+        return r.json()
+    else:
+        return "Error uploading file!" 
+
+# Test function
+@app.route('/<string:name>/<string:numba>/')
+def hello(name,numba):
+    return "Hello " + name + " your numba is " + numba
 
 
 if __name__ == '__main__':
