1556a1dbe08d6029a1f79f3e7198bd6b35c48fc9
nicoSix <nicolasix@outlook.fr>
2021-03-21 01:30:02 +0100

    Removing app from Souvenir repo : moved to Souvenir-UI

--- app/src/api/index.js
+++
@@ -1,9 +0,0 @@
-// WEB3 functions exports
-export { mintToken } from './web3';
-export { getTokenURI } from './web3';
-export { getTokenOfOwnerByIndex } from './web3';
-export { getBalanceOfAccount } from './web3';
-
-// Pinata functions exports
-export { storeImage } from './ipfs';
-export { generateAndStoreImage } from './ipfs';
--- app/src/views/NotFound/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function NotFound() {
-  return(
-    <h1>Page not found.</h1>
-  )
-}
